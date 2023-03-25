cdac5f0eb57fda0a6ca929a6951d815946d4c1bb
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-03-25 19:14:26 +0100

    skip tests

--- tests/EbookTest.php
+++ tests/EbookTest.php
@@ -6,3 +6,10 @@ it('can create an instance of Ebook', function (string $path) {
     expect($ebook)->toBeInstanceOf(Kiwilan\Ebook\Ebook::class);
     expect($ebook->path())->toBe($path);
 })->with(BOOKS_ITEMS);
+
+it('can create an instance of Ebook', function () {
+    $ebook = Kiwilan\Ebook\Ebook::make(PDF);
+
+    expect($ebook)->toBeInstanceOf(Kiwilan\Ebook\Ebook::class);
+    expect($ebook->path())->toBe(PDF);
+})->skip(PHP_OS_FAMILY === 'Windows', 'Skip on Windows');
