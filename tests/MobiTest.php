662afc24d8cdee5237f7b63befdc9ec0c099e58f
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-06-16 16:35:41 +0200

    clean

--- tests/MobiTest.php
+++ tests/MobiTest.php
@@ -4,7 +4,6 @@ use Kiwilan\Ebook\Ebook;
 
 it('can parse mobi', function () {
     $ebook = Ebook::read(STANDARD_MOBI);
-    ray($ebook->metadata()->mobi());
 
     expect($ebook->title())->toBe("Alice's Adventures in Wonderland");
     expect($ebook->authors()[0]->name())->toBe('Lewis Carroll');
