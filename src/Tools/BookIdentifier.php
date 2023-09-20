86ab734d059afbdd2ebd9f43c6fba9c3bd6b51b2
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-09-20 16:11:57 +0200

    improve book identifier

--- src/Tools/BookIdentifier.php
+++ src/Tools/BookIdentifier.php
@@ -9,35 +9,86 @@ class BookIdentifier
         protected ?string $scheme = null, // isbn10, isbn13, asin, etc.
     ) {
         $this->value = BookMeta::parse($this->value);
-
-        $scheme = $this->parseScheme();
-        $this->scheme = $scheme ?? $this->scheme;
+        $this->scheme = $this->parseScheme($this->scheme);
     }
 
-    private function parseScheme(): ?string
+    private function parseScheme(string $scheme = null): string
     {
-        if ($this->scheme === null) {
-            $regex = '/^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/';
-            if (preg_match($regex, $this->value, $matches)) {
-                $isbn = str_replace('-', '', $matches[0]);
+        $isValidInt = is_int($this->value) || ctype_digit($this->value);
+        $isValidIsbn = false;
+        if ($isValidInt && $this->isIsbn()) {
+            $scheme = $this->parseIsbn();
+            $isValidIsbn = $scheme !== null;
+        }
 
-                return (strlen($isbn) === 10)
-                    ? 'isbn10'
-                    : 'isbn13';
+        if (! $isValidIsbn) {
+            if ($this->isDoi()) {
+                $scheme = 'doi';
+            } elseif ($this->isUuid()) {
+                $scheme = 'uuid';
             }
-
-            return null;
         }
 
-        $regex = '/\b(?:ISBN(?:: ?| ))?((?:97[89])?\d{9}[\dx])\b/i';
-
-        if (preg_match($regex, str_replace('-', '', $this->value), $matches)) {
-            return (strlen($matches[1]) === 10)
-                ? 'isbn10'
-                : 'isbn13';
+        if (! $scheme) {
+            if (str_contains($this->value, ':')) {
+                $scheme = explode(':', $this->value)[0];
+            } else {
+                // assign default scheme
+                $scheme = base64_encode($this->value);
+            }
         }
 
-        return strtolower($this->scheme);
+        return strtolower($scheme);
+    }
+
+    private function isIsbn(): bool
+    {
+        $regex = '/^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+$/';
+        if (preg_match($regex, $this->value, $matches)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function isDoi(): bool
+    {
+        $regex = '/^10.\d{4,9}\/[-._;()\/:A-Z0-9]+$/i';
+        if (preg_match($regex, $this->value, $matches)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function isUuid(): bool
+    {
+        $regex = '/^urn:uuid:([a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12})$/i';
+        if (preg_match($regex, $this->value, $matches)) {
+            return true;
+        }
+
+        $regex = '/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i';
+        if (preg_match($regex, $this->value, $matches)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function parseIsbn(): ?string
+    {
+        $isbn = str_replace('-', '', $this->value);
+
+        if (strlen($isbn) === 10) {
+            return 'isbn10';
+        }
+
+        if (strlen($isbn) === 13) {
+            return 'isbn13';
+        }
+
+        return null;
     }
 
     public function getValue(): ?string
