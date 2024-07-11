e283a5dc54c9d9cbbfaf199e0f760a468097495e
ewilan-riviere <ewilan-riviere@users.noreply.github.com>
2024-07-11 10:28:57 +0000

    Fix styling

--- src/Formats/EbookParser.php
+++ src/Formats/EbookParser.php
@@ -22,8 +22,7 @@ class EbookParser
         protected ?Fb2Module $fb2 = null,
         protected ?PdfModule $pdf = null,
         protected ?string $type = null,
-    ) {
-    }
+    ) {}
 
     public static function make(EbookModule $module): self
     {
